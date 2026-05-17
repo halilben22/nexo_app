0b5fd30a3d41f504c396776accc0213a09fca6a4
halilben22 <halilattar01@gmail.com>
2026-05-17 10:08:32 +0200

    app radius class added

---
+++ lib/core/theme/app_radius.dart
@@ -0,0 +1,13 @@
+class AppRadius {
+  AppRadius._();
+
+  static const double sm = 8.0;
+
+  static const double md = 12.0;
+
+  static const double lg = 16.0;
+
+  static const double xl = 24.0;
+
+  static const double full = 999.0;
+}
