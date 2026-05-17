026ffd9c93ab1e6c288c3c92bfef098893b7d9b6
halilben22 <halilattar01@gmail.com>
2026-05-17 09:58:01 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/nexo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nexo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFF6C63FF);
+
+  static const secondary = Color(0xFF4CD964);
+  static const tertiary = Color(0xffFF3B30);
+  static const neutral = Color(0xff0F0F0F);
+
+  //background
+  static const background = Color(0xff000000);
+
+  //text
+  static const textPrimary = Color(0xffFFFFFF);
+  static const textSecondary = Color(0xff000000);
+
+  //status
+
+  static const success = Color(0xff55E16B);
+  static const warning = Color(0xffFFB4AA);
+  static const error = Color(0xff690003);
+
+  //border
+  static const border = Color(0xffF3F0EF);
+
+  //button
+  static const primaryButton = Color(0xffC4C0FF);
+  static const secondaryButton = Color(0xff313030);
+  static const invertedButton = Color(0xffE5E2E1);
+}
