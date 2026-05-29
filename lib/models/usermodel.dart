7b2d9b5615c2b3442a0c0ed8551b4b730c54b2d8
halilben22 <halilattar01@gmail.com>
2026-05-29 20:18:59 +0200

    usermodel added and polished

---
+++ lib/models/usermodel.dart
@@ -0,0 +1,51 @@
+class Usermodel {
+  final String id;
+
+  final String name;
+
+  final String surname;
+
+  final String email;
+
+  final bool isOnboardingCompleted;
+
+  final String profilePictureUrl;
+
+  final String currency;
+
+  Usermodel({
+    required this.id,
+    required this.name,
+    required this.surname,
+    required this.email,
+    required this.isOnboardingCompleted,
+    required this.profilePictureUrl,
+    required this.currency,
+  });
+
+  //FOR CONVERTING THE INCOMİNG JSON/FIRESSTORE DATA TO USERMODEL
+  factory Usermodel.fromJson(Map<String, dynamic> json) {
+    return Usermodel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      surname: json['surname'] as String,
+      email: json['email'] as String,
+      isOnboardingCompleted: json['isOnboardingCompleted'] as bool,
+      profilePictureUrl: json['profilePictureUrl'] as String,
+      currency: json['currency'] as String,
+    );
+  }
+
+  //FOR CONVERTING THE USERMODEL TO JSON/FIRESTORE DATA
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "name": name,
+      "surname": surname,
+      "email": email,
+      "isOnboardingCompleted": isOnboardingCompleted,
+      "profilePictureUrl": profilePictureUrl,
+      "currency": currency,
+    };
+  }
+}
