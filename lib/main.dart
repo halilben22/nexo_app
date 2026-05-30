01d5f28742e37a04ee06857199876dff1857992a
halilben22 <halilattar01@gmail.com>
2026-05-30 20:59:23 +0200

    firebase initialized.both firestore and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nexo_app/core/theme/app_theme.dart';
+import 'package:nexo_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
